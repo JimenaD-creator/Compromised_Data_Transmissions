62b013906939650c9f52bb108781f6f04ce8033d
JimenaD <jimenadiazfranco166@gmail.com>
2025-09-12 12:36:18 -0600

    KMP Alhorithm

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 void constructPrefix(const string& pattern, vector<int>& prefix){
     int m = pattern.length();
-    int len = 0; // Length of the previous longest prefix suffix
+    int len = 0; 
     prefix[0] = 0;
     int i = 1;
     while(i < m){
@@ -63,7 +63,7 @@ vector<int> KMPSearch(const string& pat, const string& text){
 
 }
 
-bool foundPatterns(vector<int>& positions){
+bool findPatterns(const vector<int>& positions){
     if(positions.empty()){
         return false;
     }
@@ -87,8 +87,6 @@ int main(){
 
         file_contents = buffer.str();
 
-        cout<<file_contents<<endl;
-
         stringfiles[i] = file_contents;
 
         file.close();
@@ -96,43 +94,21 @@ int main(){
         stringfiles[i].erase(remove(stringfiles[i].begin(), stringfiles[i].end(), '\n'), stringfiles[i].end());
 
 
-    }
-    vector<string> textNames = {"text1", "text2"};
-    vector<string> patternNames = {"pattern1", "pattern2", "pattern3"};
-
+    cout << " \n.......................... Search Malicious Code (Part 1).............................."<<endl;
+    vector<string> textNames = {"transmission1", "transmission2"};
+    vector<string> patternNames = {"mcode1", "mcode2", "mcode3"};
 
     for (int ti = 0; ti < 2; ti++) {
         for (int pi = 0; pi < 3; pi++) {
             vector<int> pos = KMPSearch(stringfiles[2+pi], stringfiles[ti]);
-           if(foundPatterns(pos)){
-                cout << textNames[ti] << " contains " << patternNames[pi] << " true" << " at positions: ";
+           if(findPatterns(pos)){
+                cout << patternNames[pi] << " in " << textNames[ti] << ":"<< " true" << ", at positions: ";
                 for(int p : pos) cout << p << " ";
                 cout << endl;
                 }else{
-                    cout << textNames[ti] << " does not contain " << patternNames[pi] << " false" << endl;
+                     cout << patternNames[pi] << " in " << textNames[ti] << ":"<< " false"<<endl;
                 }
         }
     }
-
-    /*string text1 = "A12B34C56D78E9F12345ABCCBA54321FEDCBAABC1234DEF5678ABBA8765FEDCBA123456";
-    string text2 = "12345ABCCBA54321FEDCBA9876543210ABCDEFFEDCBA1234567890ABCDEFABBAFEDCBA";
-    string pattern1 = "ABCCBA";
-    string pattern2 = "123456";
-    string pattern3 = "FEDCBA";
-
-
-    vector<string> textNames = {"text1", "text2"};
-    vector<string> texts = {text1, text2};
-    vector<string> patternNames = {"pattern1", "pattern2", "pattern3"};
-    vector<string> patterns = {pattern1, pattern2, pattern3};
-
-    for (int ti = 0; ti < texts.size(); ++ti) {
-        for (int pi = 0; pi < patterns.size(); ++pi) {
-            bool found = KMPSearch(patterns[pi], texts[ti]);
-            cout << textNames[ti] << " contains " << patternNames[pi] << ": " << (found ? "true": "false") << endl;
-        }
-    }
-*/
-
-
+}
 }
