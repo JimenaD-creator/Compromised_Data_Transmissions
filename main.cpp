5f97d91d10cb57e07dcfdccaf82f2c6c388b427f
Ilan Gómez Guerrero <gomezguerrero.ilan@gmail.com>
2025-09-12 11:12:32 -0600

    Dynamic Programming LCS

--- main.cpp
+++ main.cpp
@@ -1,81 +1,8 @@
 #include <iostream>
+#include <string>
 #include <vector>
-#include <fstream>
-#include <sstream>
-#include <algorithm>
+
 using namespace std;
-
-<<<<<<< Updated upstream
-int main(){
-=======
-
-void constructPrefix(const string& pattern, vector<int>& prefix){
-    int m = pattern.length();
-    int len = 0; // Length of the previous longest prefix suffix
-    prefix[0] = 0;
-    int i = 1;
-    while(i < m){
-        if(pattern[i] == pattern[len]){
-            len++;
-            prefix[i] = len;
-            i++;
-
-        }else{
-            if(len!=0){
-                len = prefix[len - 1];
-            }else{
-                prefix[i] = 0;
-                i++;
-            }
-        }
-    }
-}
-
-vector<int> KMPSearch(const string& pat, const string& text){
-    int n = text.length();
-    int m = pat.length();
-    if(m == 0) return {0};
-    vector<int>prefix(m);
-    vector<int>positions;
-    
-    constructPrefix(pat, prefix);
-
-    int i = 0, j = 0;
-    while(i < n){
-        if(text[i] == pat[j]){
-            i++;
-            j++;
-        }
-
-        if(j == m){
-            positions.push_back(i-m+1);
-            j = prefix[j-1]; 
-            
-        }
-        else if(i < n && pat[j] != text[i]){
-            if(j != 0){
-                j = prefix[j-1];
-            }else{
-                i++;
-            }
-
-        }   
-
-    }
-    return positions;
-
-}
-
-bool foundSubsequences(vector<int>& positions){
-    if(positions.empty()){
-        return false;
-    }
-    return true;
-}
-
-void longestCommonSubstring(const string& t1, const string& t2){
-    
-}
 
 struct Result {
     int length;
@@ -114,90 +41,18 @@ Result lcs(const string& s1, const string& s2) {
     return best;
 }
 
-int main(){
+int main() {
+    string s1 = "A12B34C56D78E9F12345ABCCBA54321FEDCBAABC1234DEF5678ABBA8765FEDCBA123456";
+    string s2 = "12345ABCCBA54321FEDCBA9876543210ABCDEFFEDCBA1234567890ABCDEFABBAFEDCBA";
+    
+    cout << "String 1: " << s1 << endl;
+    cout << "String 2: " << s2 << endl;
+    cout << endl;
 
->>>>>>> Stashed changes
-    string files[] = {"transmission1.txt","transmission2.txt","mscode1.txt","mscode2.txt","mscode3.txt"};
-    string stringfiles[5];
-    ifstream file;
-    stringstream buffer;
-    string file_contents;
-
-    int i;
-    for (i = 0; i < 5;i++){
-        file.open(files[i]);
-        cout<<files[i]<<endl;
-        buffer.str("");
-        buffer.clear();
-        buffer << file.rdbuf();
-
-        file_contents = buffer.str();
-
-        cout<<file_contents<<endl;
-
-        stringfiles[i] = file_contents;
-
-        file.close();
-
-        stringfiles[i].erase(remove(stringfiles[i].begin(), stringfiles[i].end(), '\n'), stringfiles[i].end());
-
-
-    }
-<<<<<<< Updated upstream
-=======
-    vector<string> textNames = {"transmission1", "transmission2"};
-    vector<string> patternNames = {"mcode1", "mcode2", "mcode3"};
-
-    vector<vector<int>> transmission1Pos;
-    vector<vector<int>> transmission2Pos;
-
-
-    for (int ti = 0; ti < 2; ti++) {
-        for (int pi = 0; pi < 3; pi++) {
-            vector<int> pos = KMPSearch(stringfiles[2+pi], stringfiles[ti]);
-            int len = stringfiles[2+pi].size(); 
-            int n = 0;
-           if(foundSubsequences(pos)) {
-                cout << textNames[ti] << " contains " << patternNames[pi] << " true";
-                for(int p : pos) {
-                    if (ti == 0){
-                        transmission1Pos.push_back({p,p+len});
-                    } else {
-                        transmission2Pos.push_back({p,p+len});
-                    }
-                } 
-                cout << endl;
-            } else {
-                cout << textNames[ti] << " does not contain " << patternNames[pi] << " false" << endl;
-            }
-        }
-    }
-
-    Result LCSResultado = lcs(stringfiles[0], stringfiles[1]);
-    cout << "Iterative DP Approach (for verification):" << endl;
-    cout << "Length of longest common substring: " << LCSResultado.length << endl;
-    cout << "In Transmission 1: positions " << LCSResultado.start1 << " to " << LCSResultado.end1 << endl;
-    cout << "In Transmission 2: positions " << LCSResultado.start2 << " to " << LCSResultado.end2 << endl;
-    /*string text1 = "A12B34C56D78E9F12345ABCCBA54321FEDCBAABC1234DEF5678ABBA8765FEDCBA123456";
-    string text2 = "12345ABCCBA54321FEDCBA9876543210ABCDEFFEDCBA1234567890ABCDEFABBAFEDCBA";
-    string pattern1 = "ABCCBA";
-    string pattern2 = "123456";
-    string pattern3 = "FEDCBA";
-
-
-    vector<string> textNames = {"text1", "text2"};
-    vector<string> texts = {text1, text2};
-    vector<string> patternNames = {"pattern1", "pattern2", "pattern3"};
-    vector<string> patterns = {pattern1, pattern2, pattern3};
-
-    for (int ti = 0; ti < texts.size(); ++ti) {
-        for (int pi = 0; pi < patterns.size(); ++pi) {
-            bool found = KMPSearch(patterns[pi], texts[ti]);
-            cout << textNames[ti] << " contains " << patternNames[pi] << ": " << (found ? "true": "false") << endl;
-        }
-    }
-    */
-
-
->>>>>>> Stashed changes
+    Result largestCommonSubstring = lcs(s1, s2);
+    cout << "Length of longest common substring: " << largestCommonSubstring.length << endl;
+    cout << "In String 1: positions " << largestCommonSubstring.start1 << " to " << largestCommonSubstring.end1 
+         << " (substring: '" << s1.substr(largestCommonSubstring.start1, largestCommonSubstring.length) << "')" << endl;
+    cout << "In String 2: positions " << largestCommonSubstring.start2 << " to " << largestCommonSubstring.end2 
+         << " (substring: '" << s2.substr(largestCommonSubstring.start2, largestCommonSubstring.length) << "')" << endl;
 }
